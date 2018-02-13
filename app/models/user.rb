ccc919dee72a7790240bde4d4d59e32c46151984
Jordan Kline <jordan.kline@gmail.com>
2018-02-13 15:58:00 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates_presence_of :username, :password
+  validates_presence_of :username
+  validates_presence_of :password
 
-  
+
 end
