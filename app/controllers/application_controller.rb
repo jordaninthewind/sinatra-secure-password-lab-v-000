aa4a1a7fcab34ff81765cb738e7b8e050e627bc4
Jordan Kline <jordan.kline@gmail.com>
2018-02-13 16:07:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,11 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    binding.pry
+
     @user = User.new(:username => params[:username], :password => params[:password])
 
     if @user.save
-        
+
         redirect to "/login"
     else
         redirect to "/failure"
