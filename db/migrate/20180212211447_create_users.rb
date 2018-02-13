eb5092f23b39dc05fc920ce83b2b30523ff1d0e5
Jordan Kline <jordan.kline@gmail.com>
2018-02-13 15:52:24 +0000

    automatically backed up by learn

--- db/migrate/20180212211447_create_users.rb
+++ db/migrate/20180212211447_create_users.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |u|
-      u.string :username
-      u.string :password
-      u.float :balance
+        create_table :users do |t|
+            t.string :username
+            t.string :password_digest
+        end
     end
-  end
 end
