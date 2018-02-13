1b127d9a0e7c22175a483e860494a32c8a80bc23
Jordan Kline <jordan.kline@gmail.com>
2018-02-13 16:15:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    raise params.inspect
+    binding.pry
     @user = User.new(:username => params[:username], :password => params[:password])
     binding.pry
     if @user.save
